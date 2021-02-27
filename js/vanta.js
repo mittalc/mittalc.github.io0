ed00fdce7fc84202c607a3bb483c22144048e453
mittalc <55461354+mittalc@users.noreply.github.com>
2021-02-27 12:48:44 -0500

    update

--- js/vanta.js
+++ js/vanta.js
@@ -3,9 +3,12 @@ VANTA.BIRDS({
     mouseControls: true,
     touchControls: true,
     gyroControls: false,
-    minHeight: 200.00,
-    minWidth: 200.00,
+    minHeight: 100.00,
+    minWidth: 100.00,
     scale: 1.00,
     scaleMobile: 1.00,
-    backgroundColor: 0x0
+    backgroundColor: 0x0,
+    
+
+
   })
